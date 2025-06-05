8c0e23d34bd19fa7fd3c317d3efc00334b257181
Akshita <akshitapise710@gmail.com>
2025-06-05 12:26:21 +0530

    Initial Commit

--- src/ex_01_Java_Basics/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -4,6 +4,6 @@ public class Lab001_HelloWorld {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        System.out.println("Hello World Program");
     }
 }
